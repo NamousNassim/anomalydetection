40565480d30dcbbd1a11a3cc77fcbd04b5ab5d7b
NamousNassim <nassimnamousdev@gmail.com>
2024-11-02 10:50:27 +0100

    first commit

---
+++ src/main/java/com/adet/anomalydetection/AnomalydetectionApplication.java
@@ -0,0 +1,13 @@
+package com.adet.anomalydetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnomalydetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnomalydetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adet/anomalydetection/AnomalydetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adet.anomalydetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnomalydetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
